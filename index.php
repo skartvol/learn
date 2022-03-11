c0a1038e57798eb4c5f09f3911bee8eccfcda831
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-11 10:02:43 +0200

    mathematical operators

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$a = 11;
+$b = 8;
+echo "a = $a<br>";
+echo "b = $b<br>";
+echo "Dodawanie = ".($a + $b)."<br>";
+echo "Odejmowanie = ".($a - $b)."<br>";
+echo "Umnozenie = ".($a * $b)."<br>";
+echo "Dzielenie = ".($a / $b)."<br>";
+echo "Reszta od dzielenia = ".($a % $b)."<br>";
+
+echo "Okruglenie :". round($a / $b,2);
+?>

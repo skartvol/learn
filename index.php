9c53d7cceb9b7c3f566b17e8cc95a2760eb6ccd8
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-11 09:02:05 +0200

    Data types, echo, var_damp

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 	define("MY_TITLE", "Hello ");
 	$myTitle = "World";
 	echo MY_TITLE.$myTitle;
+	echo "<br>";
+	$text = "ABCD";
+	$int = 123;
+	$float = 23.45;
+	$bool = true;
+
+	echo "$text, $int, $float, $bool";
+	echo "<br>";
+	var_dump($bool);
 ?>

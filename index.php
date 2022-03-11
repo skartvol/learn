1a99696789ea89245bd78d3cf2323a1a470aeee4
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-11 19:15:59 +0200

    pow, sqrt, post-pre incrimination

--- index.php
+++ index.php
@@ -9,5 +9,25 @@ echo "Umnozenie = ".($a * $b)."<br>";
 echo "Dzielenie = ".($a / $b)."<br>";
 echo "Reszta od dzielenia = ".($a % $b)."<br>";
 
-echo "Okruglenie :". round($a / $b,2);
+echo "Okruglenie :". round($a / $b,2)."<br>";
+
+
+$a = $a + $a;
+echo $a."<br>";
+$a += $a;
+echo $a."<br>";
+$a -= 10;
+echo $a."<br>";
+$a *= 10;
+echo $a."<br>";
+$a /= 2;
+echo $a."<br>";
+$a++;
+echo $a."<br>";
+++$a;
+echo $a."<br><br><br>";
+
+echo pow(5, 3)."<br>";
+
+echo sqrt(144);
 ?>

d5124b4e0110154903a3b83bac65bf6050271692
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-15 20:49:11 +0100

    arrar

---
+++ lesson8.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Lesson #7</title>
+    </head>
+    <body>
+    <a href="index.php">MAIN MENU</a><hr>
+
+    <?php
+        $tabs = ['Adam','Artem','Bogdan'];
+        var_dump($tabs);
+        echo "<br>";
+        $tabs_1 = array(
+            'red'   => 'Червоний',
+            'green' => 'Зелений',
+            'black' => 'Чорний'
+        );
+        print_r($tabs_1);
+
+    ?>
+
+    </body>
+</html>

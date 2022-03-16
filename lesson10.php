6841092455f47f9a6477d57ecb4f3dbb3eb28189
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-16 21:41:53 +0100

    Arithmetic mean

--- lesson10.php
+++ lesson10.php
@@ -1,19 +1,32 @@
 <html>
     <head>
-        <title>Lesson #9</title>
+        <title>Lesson #10</title>
     </head>
     <body>
-    <a href="index.php">MAIN MENU</a><hr>
-
-    <?php
-        $tabs = ['Adam','Artem','Bogdan','Jakub'];
-        foreach ($tabs as $tab) {
-            echo $tab.'<br>';
+        <a href="index.php">MAIN MENU</a><hr>
+        <h3>Arithmetic mean</h3>
+        <p>Please enter your numbers through a comma.</p>
+        
+        <form action="lesson10.php" method="get">
+            <input type="text" name="nums">
+            <input type="submit" value="OK">
+        </form>
+        <?php
+        if(isset($_GET['nums']) AND !empty($_GET['nums'])) {
+            $nums = explode(',',$_GET['nums']);
+            $numsCount = 0;
+            $result = null;
+            foreach($nums as $num) {
+                if(is_numeric($num)){
+                    $result += $num;
+                    $numsCount++;
+                }
+            }
+            echo 'Your result: '.$result/$numsCount;
+        } else {
+            echo 'Please, enter info.';
         }
-        foreach ($tabs as $key => $tab) {
-            echo $key.' => '.$tab.'<br>';
-        }
-    ?>
+        ?>
 
     </body>
 </html>

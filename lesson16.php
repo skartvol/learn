2343881e517781de43bfe512dfb2e2f902ab41b5
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-03-19 09:19:40 +0100

    Small game

--- lesson16.php
+++ lesson16.php
@@ -1,3 +1,7 @@
+<?php
+if(!isset($_SESSION))
+    session_start();
+?>
 <html>
     <head>
         <title>Lesson #16</title>
@@ -5,15 +9,38 @@
     <body>
     <a href="index.php">MAIN MENU</a><hr>
 
-    <h3>COOKIE</h3>
+    <h3>GAME - guess the number from 1 to 10</h3>
     <table>
         <tr>
             <td>
-                <form action="lesson14.php" method="POST">
-                    <h3>Cookies</h3>
-                    <p>Set coockie: <input type="text" name="cooka"></p>
+                <form action="lesson16.php" method="POST">
+                    <p>Set number: <input type="number" name="num"></p>
                     <input type="submit" value="Set">
                 </form>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php
+                if(isset($_POST['num']) and is_numeric($_POST['num'])) {
+                    if(isset($_SESSION['i']))
+                        $_SESSION['i']++;
+                    else
+                        $_SESSION['i'] = 1;
+                    
+                    if(!isset($_SESSION['secret-num']))
+                        $_SESSION['secret-num'] = random_int(1,10);
+
+                    if($_POST['num']>$_SESSION['secret-num'])
+                        echo 'Your number is more than the secret number. Please, try again.';
+                    elseif($_POST['num']<$_SESSION['secret-num'])
+                        echo 'Your number is smaller than the secret number. Please, try again.';
+                    else {
+                        echo 'Congratulations, you guessed the secret number - '.$_SESSION['secret-num'].' on the '.$_SESSION['i'].' try';
+                        session_destroy();
+                    }
+                }
+                ?>
             </td>
         </tr>
     </table>

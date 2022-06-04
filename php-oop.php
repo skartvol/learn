f0de59a4ec56bc1b86392997f45ec0d477771277
Artem Sklyanchuk <sklyanchuk.artem@gmail.com>
2022-06-04 15:24:46 +0200

    Reorganization index.php

--- php-oop.php
+++ php-oop.php
@@ -1,5 +1,30 @@
 <?php
 class ShopProduct
 {
-    
+    public $author;
+    public $bookName;
+    public $price;
+
+    public function __construct(
+        $author,
+        $bookName,
+        $price
+    ) {
+        $this->author = $author;
+        $this->bookName = $bookName;
+        $this->price = $price
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
 }
+
+$product = new ShopProduct(
+    "Мет Зандстра",
+    "PHP Об\'єкти, шаблони, методики програмування",
+    1500
+);
+
+print $product->getAuthor;
